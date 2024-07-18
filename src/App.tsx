e3992b59c117398f5842ba365b6011f21c55f129
TheIlyosTV <hakkermaktabi19@gmail.com>
2024-07-19 02:18:07 +0500

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import SiteHeader from '@/components/SiteHeader'
-import { Button } from '@/components/ui/button'
+import SiteHeader from "@/components/SiteHeader";
+import { Button } from "@/components/ui/button";
+import Drinks from "@/components/Drinks";
 
 const App = () => {
-	return (
-		<div>
-			<SiteHeader />
-			<Button>Knopka</Button>
-			<Button className='rounded-full' size='icon'>
-				+
-			</Button>
-		</div>
-	)
-}
+  return (
+    <div>
+      <SiteHeader />
+      <Button>Knopka</Button>
+      <Button className="rounded-full" size="icon">
+        +
+      </Button>
 
-export default App
+      <Drinks />
+    </div>
+  );
+};
+
+export default App;
