4c3bc489fe7a03148a209a67d87fb0f06f64ca24
shakhbozbekusmonov <miracle_0319@bk.ru>
2024-07-22 16:12:04 +0500

    fix: error

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
 import SiteHeader from '@/components/SiteHeader'
-
-import { Button } from '@/components/ui/button'
-import Kombo from '@/components/Kombo'
-
 import { ToastContainer } from 'react-toastify'
-
-
 
 const App = () => {
 	return (
 		<>
 			<SiteHeader />
 
-			<Button>Knopka</Button>
-			<Button className='rounded-full' size='icon'>
-				+
-			</Button>
-			<Kombo/>
-		</div>
-
-
 			<ToastContainer />
 		</>
-
 	)
 }
 
