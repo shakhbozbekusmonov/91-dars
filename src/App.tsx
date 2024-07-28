ea7e0e9557c16fa9fc766234fa372320036db1a9
Axadjon18 <mannonovaxadjon18@gmail.com>
2024-07-28 18:34:47 +0500

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import Counter from '@/components/Counter'
 import { CounterContext } from '@/context/counter.provider'
 import { useContext } from 'react'
 import { ToastContainer } from 'react-toastify'
+import SiteHeader from './components/SiteHead'
 
 const App = () => {
 	const { number } = useContext(CounterContext)
 
 	return (
 		<>
+			<SiteHeader />
+
 			<h1>{number}</h1>
 			<Counter />
 
---
+++ src/components/mock/nav-list.ts
@@ -0,0 +1,19 @@
+interface NavItem {
+    label: string
+    path: string
+}
+
+export const navList: NavItem[] = [
+	{
+		label: 'Menyu',
+		path: '/menyu'
+	},
+	{
+		label: 'Bolalar uchun',
+		path: '/for-children'
+	},
+	{
+		label: 'Filiallar',
+		path: '/branchs'
+	}
+]
